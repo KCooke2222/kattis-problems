da57b11b7fb07d5249bbd1fbc4b3832510f5ff96
Kevin Cooke <kevinc222@outlook.com>
2025-01-21 13:44:15 -0600

    Create repo

---
+++ Complete/Öfugsnúið.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    // Get numbers in array
+    int numbers[] = {};
+    
+    // Sort array
+    int input;
+    
+    while input
+    scanf("%d", &numbers)
+    
+    // Check for duplicates
+}
+
+
